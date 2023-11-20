b880760a6ee2f9757341eee6ec85ebf69929f5d7
Carlos Cortez <thehome@live.com.ar>
2023-11-20 18:02:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MainRoutes from "./routes/MainRouter";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <MainRoutes />
+    </div>
+  );
+}
+
+export default App;
